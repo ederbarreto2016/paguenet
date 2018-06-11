82b9b21e0ef3099990ef02065f001bf1997ad7e8
Éder Barreto <eder.barreto.oliveira@hotmail.com>
2018-06-11 08:51:53 -0300

    Configuração Mysql

--- src/main/java/com/paguenet/DataConfiguration.java
+++ src/main/java/com/paguenet/DataConfiguration.java
@@ -16,19 +16,23 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 	
-	 @Bean
-	    public BasicDataSource dataSource() throws URISyntaxException {
-	        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-	        String username = dbUri.getUserInfo().split(":")[0];
-	        String password = dbUri.getUserInfo().split(":")[1];
-	        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-
-	        BasicDataSource basicDataSource = new BasicDataSource();
-	        basicDataSource.setUrl(dbUrl);
-	        basicDataSource.setUsername(username);
-	        basicDataSource.setPassword(password);
-
-	        return basicDataSource;
-	    }
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/paguenet");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 }
